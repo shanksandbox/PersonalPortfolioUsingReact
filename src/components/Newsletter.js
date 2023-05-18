3f9ecc6425018b5701126911605788b640072a17
Prakhar Singh <singhprakhar359@gmail.com>
2023-05-18 19:09:50 +0530

    init

---
+++ src/components/Newsletter.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import { Col, Row, Alert } from "react-bootstrap";
+
+export const Newsletter = ({ status, message, onValidated }) => {
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    if (status === 'success') clearFields();
+  }, [status])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    email &&
+    email.indexOf("@") > -1 &&
+    onValidated({
+      EMAIL: email
+    })
+  }
+
+  const clearFields = () => {
+    setEmail('');
+  }
+
+  return (
+      <Col lg={12}>
+        <div >
+         
+        </div>
+      </Col>
+  )
+}
